e23a3608c455b632cfd330e0826a144ec5326fa6
larry.adams <ladams1776@yahoo.com>
2019-09-11 21:35:39 -0400

    Create Controller

---
+++ src/main/java/com/microservices/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,12 @@
+package com.microservices.limitsservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsConfigurationController {
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitsFromConfigurations() {
+        return new LimitConfiguration(1, 1000);
+    }
+}
